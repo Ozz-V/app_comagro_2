27e19752a738a778d545fb02d21a7944c97eea39
Ozz-V <oevilla.jip@gmail.com>
2026-06-29 12:10:42 -0300

    Security fixes, cleanup & remove sharp

--- supabase/functions/get-key/index.ts
+++
@@ -1,4 +0,0 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
-serve(async (req) => {
-    return new Response(JSON.stringify({ key: Deno.env.get('GEMINI_API_KEY') }), { headers: { 'Content-Type': 'application/json' } });
-});
