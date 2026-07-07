06e54c779f6abbb3e77d63da9edb072c8677b8b8
Ozz-V <oevilla.jip@gmail.com>
2026-07-07 17:52:32 -0300

    chore(refactor): Fase 1 - Cimientos (TS, ESLint, Supabase Types, CI Gates, Limpieza)

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: [
+    'expo',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error'
+  }
+};
