afeb73d78cb32367b63b2e884c19072e5b806533
Ozz-V <oevilla.jip@gmail.com>
2026-07-10 11:05:27 -0300

    fix: update test import to use @testing-library/react-native

--- tests/useProducts.test.ts
+++ tests/useProducts.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-native';
 import { useProducts } from '../src/hooks/useProducts';
 import { useOfflineSync } from '../src/contexts/OfflineSyncContext';
 import { initDB, searchProducts, getUniqueBrands, getProductBySku } from '../src/utils/database';
